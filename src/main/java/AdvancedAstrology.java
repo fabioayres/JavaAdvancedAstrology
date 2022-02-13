f042e69c0a67236c3555329a5b5c8c28c623cf70
Fabio Ayres <fabinhoayres@gmail.com>
2022-02-13 13:30:41 -0300

    Add printStars() and printSpaces() method

--- src/main/java/AdvancedAstrology.java
+++ src/main/java/AdvancedAstrology.java
@@ -2,11 +2,17 @@
 public class AdvancedAstrology {
 
     public static void printStars(int number) {
-        // part 1 of the exercise
+        for (int i = 1; i<= number; i++){
+            System.out.print("*");
+        }
+        System.out.println("");
     }
 
     public static void printSpaces(int number) {
         // part 1 of the exercise
+        for (int i = 1; i<= number; i++){
+            System.out.print(" ");
+        }
     }
 
     public static void printTriangle(int size) {
@@ -20,10 +26,14 @@ public class AdvancedAstrology {
     public static void main(String[] args) {
         // The tests are not checking the main, so you can modify it freely.
 
-        printTriangle(5);
-        System.out.println("---");
-        christmasTree(4);
-        System.out.println("---");
-        christmasTree(10);
+//        printTriangle(5);
+//        System.out.println("---");
+//        christmasTree(4);
+//        System.out.println("---");
+//        christmasTree(10);
+        printSpaces(10);    
+        printStars(4);
+        printSpaces(4);    
+        printStars(10);
     }
 }
